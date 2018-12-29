b608fccb63037a23d0213f402d5dba06d468c54b
lukiee <sonilokesh24@gmail.com>
2018-12-29 22:36:25 +0530

    Server Scriptting Done

--- db.js
+++ db.js
@@ -7,4 +7,35 @@ const url = "mongodb://localhost:27017";
 const mongoOptions = {
   userNewUrlPraser: true
 };
-const 
+const state = {
+  db: null
+};
+
+const connect = cb => {
+  if (state.db) {
+    cb();
+  } else {
+    MongoClient.connect(
+      url,
+      mongoOptions,
+      (err, client) => {
+        if (err) {
+          cb(err);
+        } else {
+          state.db = client.db(dbname);
+          cb();
+        }
+      }
+    );
+  }
+};
+
+const getPrimaryKey = _id => {
+  return ObjectID(_id);
+};
+
+const getDB = () => {
+  return state.db;
+};
+
+module.exports = { getDB, getPrimaryKey, connect };
